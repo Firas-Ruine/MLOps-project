dbf9c98bac0a3afc73c076db24368560ff518bd9
Firas Ruine <firas.ruine@maisonduweb.com>
2024-12-11 23:24:36 +0100

    remove unused configuration files and assets

--- src/data_ingestion/facebook_comments/utils/facebook_comments_classification.py
+++
@@ -1,4 +0,0 @@
-from utils.get_comments_classification import getCommentsClassification
-
-def main(classification_input, classification_result):
-    getCommentsClassification(classification_input, classification_result)
--- src/data_ingestion/facebook_comments/utils/facebook_comments_scraping.py
+++
@@ -1,5 +0,0 @@
-from utils.get_posts_comments import getPostsComments
-
-
-def main(scraping_input, scraping_result):
-    getPostsComments(scraping_input, scraping_result)
